7668a72a1ce7005c3d12f3b1d956a48da8e45682
Masud76301 <khaledmasud76301@gmail.com>
2026-02-24 22:28:09 +0600

    alljob script done

--- script/allJobs.js
+++ script/allJobs.js
@@ -1,6 +1,103 @@
+let jobInterviewList = [];
+let jobRejectedList = [];
 
+
+
+// Interview list card add 
 document.getElementById('all-job')
     .addEventListener('click', function (event) {
-        
+
+        if (event.target.classList.contains('interview-btn')) {
+            const parentNode = event.target.parentNode.parentNode;
+
+            const companyName = parentNode.querySelector('.company-name').innerText;
+            const jobPosition = parentNode.querySelector('.job-position').innerText;
+            const jobLocation = parentNode.querySelector('.job-location').innerText;
+            const jobType = parentNode.querySelector('.job-type').innerText;
+            const salary = parentNode.querySelector('.salary').innerText;
+            const jobDescription = parentNode.querySelector('.job-description').innerText;
+
+            const cardInfo = {
+                companyName,
+                jobPosition,
+                jobLocation,
+                jobType,
+                salary,
+                jobDescription
+
+            }
+
+            const cardExit = jobInterviewList.find(item => item.companyName == cardInfo.companyName)
+
+
+            if (!cardExit) {
+                jobInterviewList.push(cardInfo);
+            }
+            interviewListAdded();
+            count();
+        }
 
     })
+
+function interviewListAdded() {
+
+    interviewCardList.innerHTML = "";
+
+    for (let interview of jobInterviewList) {
+        let div = document.createElement('div');
+        div.className = 'card bg-base-200 card-xs shadow-sm w-full  flex-row justify-between items-start p-6'
+
+        div.innerHTML = `
+             <!-- Card Contain -->
+                <div class="w-[90%] ">
+
+                    <h1 class="company-name text-[18px] font-semibold text-black">${interview.companyName}</h1>
+                    <p class="job-position mb-[20px] text-[16px] font-normal text-gray-400">React Native Developer</p>
+
+                    <ul class="flex gap-3 mb-[20px] text-[14px] font-normal text-gray-400 ">
+                        <li class="job-location">Remote</li>
+                        <li>•</li>
+                        <li class="job-type">Full-time </li>
+                        <li>•</li>
+                        <li class="salary"> $130,000 - $175,000 </li>
+                    </ul>
+
+                    <p
+                        class="job-status w-[120px] mb-2 rounded-sm text-[14px] font-medium text-black px-3 py-2 bg-[#EEF4FF]">
+                        NOT
+                        APPLIED</p>
+
+                    <p class="job-description mb-[20px] text-[14px] font-normal text-[#323B49]">Build cross-platform
+                        mobile applications
+                        using React Native. Work on products used by millions of users worldwide.</p>
+
+                    <div class="flex justify-start gap-2">
+                        <button class="interview-btn btn btn-outline btn-success">INTERVIEW</button>
+                        <button class="rejected-btn btn btn-outline btn-error">REJECTED</button>
+                    </div>
+                </div>
+
+                <!-- Deleted Button -->
+                <button class="del-btn btn btn-link hover:text-error no-underline">
+                    <i class="del-icon fa-regular fa-trash-can"></i>
+                </button>
+            `
+        interviewCardList.appendChild(div);
+    }
+}
+
+
+// Interview list card deleted 
+
+
+document.getElementById('interview-list')
+    .addEventListener('click', function (event) {
+        if (event.target.classList.contains('del-icon')) {
+            const siblingNode = event.target.parentNode.parentNode;
+            const companyName = siblingNode.querySelector('.company-name').innerText;
+
+           jobInterviewList  = jobInterviewList.filter(item => item.companyName != companyName)
+        }
+        
+    })
+
--- script/count.js
+++ script/count.js
@@ -1,21 +1,32 @@
 const allJobCount = document.getElementById('all-job');
-const allInterviewCount = document.getElementById('interview-list');
+const allInterviewCount = document.getElementById('interview-card-list');
 const allRejectedCount = document.getElementById('rejected-list');
+const interviewCardList = document.getElementById('interview-card-list');
 
-const totalJob = allJobCount.children.length - 1;
-const totalInterview = allInterviewCount.children.length - 1;
-const totalRejected = allRejectedCount.children.length - 1;
+count();
 
-document.getElementById('total-job').innerText = totalJob;
-document.getElementById('job-total').innerText = totalJob;
-document.getElementById('total-interview').innerText = totalInterview;
-document.getElementById('interview-total').innerText = totalInterview;
-document.getElementById('total-reject').innerText = totalRejected;
-document.getElementById('rejected-total').innerText = totalRejected;
+function count() {
+    totalJob = allJobCount.children.length - 1;
+    totalInterview = allInterviewCount.children.length;
+    totalRejected = allRejectedCount.children.length - 1;
 
-if (totalInterview != 0) {
+    console.log("why not working");
+
+    document.getElementById('total-job').innerText = totalJob;
+    document.getElementById('job-total').innerText = totalJob;
+    document.getElementById('total-interview').innerText = totalInterview;
+    document.getElementById('interview-total').innerText = totalInterview;
+    document.getElementById('total-reject').innerText = totalRejected;
+    document.getElementById('rejected-total').innerText = totalRejected;
+
+    if (totalInterview != 0) {
+     document.getElementById('no-interview-list').classList.remove('flex');
     document.getElementById('no-interview-list').classList.add('hidden');
+   
 }
+}
+
+
 
 if (totalRejected != 0) {
     document.getElementById('no-rejected-list').classList.add('hidden');
@@ -44,8 +55,8 @@ document.getElementById('interview-list')
     .addEventListener('click', function (event) {
         if (event.target.classList.contains('del-icon')) {
 
-            const allJob = document.getElementById('interview-list');
-            const totalJob = allJob.children.length - 2;
+            const allJob = document.getElementById('interview-card-list');
+            const totalJob = allJob.children.length - 1;
             document.getElementById('total-interview').innerText = totalJob;
             document.getElementById('interview-total').innerText = totalJob;
 
